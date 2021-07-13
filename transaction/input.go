f67008bf441f151bbbd27482e869f57142ed44b4
adisaputra7 <nurhadisaputra7@gmail.com>
2021-07-13 21:03:50 +0700

    Fixing typo transaction

--- transaction/input.go
+++ transaction/input.go
@@ -15,7 +15,7 @@ type CreateTransactionInput struct {
 
 type TransactionNotificationInput struct {
 	TransactionStatus string `json:"transaction_status"`
-	OrderID           string `json:"oreder_id"`
+	OrderID           string `json:"order_id"`
 	PaymentType       string `json:"payment_type"`
 	FraudStatus       string `json:"fraud_status"`
 }
